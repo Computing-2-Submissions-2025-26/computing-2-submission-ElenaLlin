6226a4387663c345484ec3142003c294d5bfe0b2
ElenaLlin <ellinper@btinternet.com>
2026-06-25 13:50:16 +0100

    board coordinates

--- web-app/main.js
+++ web-app/main.js
@@ -1,28 +1,185 @@
 import R from "./ramda.js";
 import GooseLudo from "./GooseLudo.js";
 
-// table things, stuff for the UI
-const row_count = 20;
-const column_count = 20;
 
-document.documentElement.style.setProperty("--row-count", row_count);
-document.documentElement.style.setProperty("--column-count", column_count);
+const game_board = document.getElementById("game_board");
 
-const game_grid = document.getElementById("game_board");
+const boardCoords = {
+    homePositions: {
+        red: [
+            { x: 165, y: 165 },
+            { x: 225, y: 165 },
+            { x: 165, y: 225 },
+            { x: 225, y: 225 }
+        ],
+        yellow: [
+            { x: 945, y: 945 },
+            { x: 1005, y: 945 },
+            { x: 945, y: 1005 },
+            { x: 1005, y: 1005 }
+        ],
+        green: [
+            { x: 165, y: 945 },
+            { x: 225, y: 945 },
+            { x: 165, y: 1005 },
+            { x: 225, y: 1005 }
+        ],
+        blue: [
+            { x: 945, y: 165 },
+            { x: 1005, y: 165 },
+            { x: 945, y: 225 },
+            { x: 1005, y: 225 }
+        ]
+    },
 
-R.range(0, row_count).forEach(function (row_index) {
-    const tr = document.createElement("tr");
-    game_grid.append(tr);
+    mainPath: [
+        { x: 720, y: 1160 },
+        { x: 720, y: 1100 },
+        { x: 720, y: 1040 },
+        { x: 720, y: 980 },
+        { x: 720, y: 920 },
+        { x: 720, y: 860 },
+        { x: 720, y: 800 },
+        { x: 720, y: 740 },
 
-    R.range(0, column_count).forEach(function (column_index) {
-        const td = document.createElement("td");
-        tr.append(td);
-        td.textContent = `[${row_index}, ${column_index}]`;
+        { x: 740, y: 720 },
+        { x: 800, y: 720 },
+        { x: 860, y: 720 },
+        { x: 920, y: 720 },
+        { x: 980, y: 720 },
+        { x: 1040, y: 720 },
+        { x: 1100, y: 720 },
 
-        td.onclick = function () {
-            document.querySelector("aside").textContent = (
-                `Row: ${row_index}, Column: ${column_index}`
-            );
-        };
+        { x: 1160, y: 720 },
+        { x: 1160, y: 580 },
+
+        { x: 1160, y: 460 },
+        { x: 1100, y: 460 },
+        { x: 1040, y: 460 },
+        { x: 980, y: 460 },
+        { x: 920, y: 460 },
+        { x: 860, y: 460 },
+        { x: 800, y: 460 },
+
+        { x: 750, y: 460 },
+        { x: 720, y: 440 },
+        { x: 720, y: 380 },
+        { x: 720, y: 320 },
+        { x: 720, y: 260 },
+        { x: 720, y: 200 },
+        { x: 720, y: 140 },
+        { x: 720, y: 80 },
+
+        { x: 720, y: 20 },
+        { x: 590, y: 20 },
+
+        { x: 460, y: 20 },
+        { x: 460, y: 80 },
+        { x: 460, y: 140 },
+        { x: 460, y: 200 },
+        { x: 460, y: 260 },
+        { x: 460, y: 320 },
+        { x: 460, y: 380 },
+        { x: 460, y: 440 },
+
+        { x: 430, y: 460 },
+        { x: 370, y: 460 },
+        { x: 310, y: 460 },
+        { x: 250, y: 460 },
+        { x: 190, y: 460 },
+        { x: 130, y: 460 },
+        { x: 70, y: 460 },
+
+        { x: 10, y: 460 },
+        { x: 10, y: 590 },
+
+        { x: 10, y: 720 },
+        { x: 70, y: 720 },
+        { x: 130, y: 720 },
+        { x: 190, y: 720 },
+        { x: 250, y: 720 },
+        { x: 310, y: 720 },
+        { x: 370, y: 720 },
+        { x: 430, y: 720 },
+
+        { x: 460, y: 740 },
+        { x: 460, y: 800 },
+        { x: 460, y: 860 },
+        { x: 460, y: 920 },
+        { x: 460, y: 980 },
+        { x: 460, y: 1040 },
+        { x: 460, y: 1100 },
+        { x: 460, y: 1160 },
+        { x: 580, y: 1160 }
+
+    ],
+
+    finalPath: {
+        red: [
+            { x: 590, y: 70 },
+            { x: 590, y: 130 },
+            { x: 590, y: 190 },
+            { x: 590, y: 250 },
+            { x: 590, y: 310 },
+            { x: 590, y: 370 },
+            { x: 590, y: 430 }
+        ],
+        yellow: [
+            { x: 590, y: 1100 },
+            { x: 590, y: 1040 },
+            { x: 590, y: 980 },
+            { x: 590, y: 920 },
+            { x: 590, y: 860 },
+            { x: 590, y: 800 },
+            { x: 590, y: 740 }
+        ],
+        green: [
+            { x: 70, y: 590 },
+            { x: 130, y: 590 },
+            { x: 190, y: 590 },
+            { x: 250, y: 590 },
+            { x: 310, y: 590 },
+            { x: 370, y: 590 },
+            { x: 430, y: 590 }
+
+        ],
+        blue: [
+            { x: 740, y: 590 },
+            { x: 800, y: 590 },
+            { x: 860, y: 590 },
+            { x: 920, y: 590 },
+            { x: 980, y: 590 },
+            { x: 1040, y: 590 },
+            { x: 1100, y: 590 },
+        ]
+    },
+
+    safeSquares: GooseLudo.safeSquares
+};
+
+
+
+function loadBoardSVG() {
+    return new Promise((resolve, reject) => {
+        fetch("board.svg")
+            .then(response => response.text())
+            .then(svgContent => {
+                document.getElementById("boardContainer").innerHTML = svgContent;
+                boardSVG = document.getElementById("boardContainer").querySelector("svg");
+
+                if (!boardSVG.getAttribute("viewBox")) {
+                    boardSVG.setAttribute("viewBox", "0 0 800 800");
+                }
+
+                boardReady = true;
+                resolve();
+            })
+            .catch(error => {
+                console.error("Error loading SVG:", error);
+                document.getElementById("boardContainer").innerHTML =
+                    "<p style="color: red; ">Error loading board.svg</p>";
+                reject(error);
+            });
     });
-});
+}
+
